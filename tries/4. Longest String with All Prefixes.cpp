579ca843c9dcd60ae48849fe2028ca02d00d15a2
Dipali Kumbhar <63409376+Dipali742@users.noreply.github.com>
2024-04-03 20:47:57 +0530

    Update 4. Longest String with All Prefixes.cpp

--- tries/4. Longest String with All Prefixes.cpp
+++ tries/4. Longest String with All Prefixes.cpp
@@ -12,7 +12,7 @@ None
 
 /*
   Naive approach using map
-  T(C) => O(n*[length of longest string in array])
+  T(C) => O(n*[average length of strings in array])
   S(C) => O(n)
 */
 string completeString(int n, vector<string> &a){
@@ -44,3 +44,86 @@ string completeString(int n, vector<string> &a){
 
         return ans != "" ? ans : "None";
 }
+
+
+/*
+  using trie data structure.
+  T(C) and S(C) are almost same. There is difference of constants. 
+*/
+
+#include <bits/stdc++.h> 
+
+class Node {
+private:
+    Node* links[26];
+    bool endFlag = false;
+
+public:
+    bool containsKey(char ch) {
+        return links[ch-'a'] != NULL;
+    }
+
+    void putKey(char ch) {
+        links[ch-'a'] = new Node();
+    }
+
+    Node* getKey(char ch) {
+        return links[ch-'a'];
+    }
+
+    void isEnd() {
+        endFlag = true;
+    }
+
+    bool isEndChar() {
+        return endFlag;
+    }
+};
+
+class Trie {
+private:
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string word) {
+        Node* node = root;
+        for(int i=0;i<word.length();i++) {
+            if(!node->containsKey(word[i])) {
+                node->putKey(word[i]);
+            }
+            node = node->getKey(word[i]);
+        }
+        node->isEnd();
+    }   
+
+    bool checkIfPrefix(string word) {
+        Node* node = root;
+        for(int i=0;i<word.length();i++) {
+            node = node->getKey(word[i]);
+            if(!node->isEndChar())
+                return false;
+        }
+        return true;
+    } 
+};
+
+string completeString(int n, vector<string> &a){
+    Trie trie;
+    for(int i=0;i<n;i++) {
+        trie.insert(a[i]);
+    }
+
+    string ans = "";
+    for(int i=0;i<n;i++) {
+        if(trie.checkIfPrefix(a[i])) {
+            if(ans.length() < a[i].length())
+                ans = a[i];
+            else if(ans.length() == a[i].length() && ans > a[i])
+                ans = a[i];
+        }
+    }
+    return ans!="" ? ans : "None";
+} 
