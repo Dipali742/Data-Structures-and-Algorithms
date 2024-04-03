61dc042b81862f81294d701e62e1a63372bd5854
Dipali Kumbhar <63409376+Dipali742@users.noreply.github.com>
2024-04-03 21:57:37 +0530

    Update 5. Number of distinct substrings.cpp

--- tries/5. Number of distinct substrings.cpp
+++ tries/5. Number of distinct substrings.cpp
@@ -1,8 +1,8 @@
 /*
   Naive approach -> find all substrings and store in set 
-  T(C) => O(n^3)
-  S(C) => O(n^2)
-  n is the length of the input string s
+  T(C) => O(n^2 * log(m))  
+  S(C) => O(n^2 * n/2) => O(n^3)
+  m is the size of the set, n is the length of the input string s
 */
 
 int countDistinctSubstrings(string &s)
@@ -17,4 +17,63 @@ int countDistinctSubstrings(string &s)
 
     }
     return st.size()+1;
+}
+
+/*
+  Trie approach - to reduce space
+  T(C) => O(n^2)
+  S(C) => 26*26*26 => approximately n(n+1)/2 nodes (one node for each substring character)
+*/
+
+class Node {
+private:
+    Node* links[26];
+public:
+    bool containsKey(char ch) {
+        return links[ch-'a'] != NULL;
+    }
+
+    void putKey(char ch, Node* node) {
+        links[ch-'a'] = node;
+    }
+
+    Node* getNode(char ch) {
+        return links[ch-'a'];
+    }
+};
+
+class Trie {
+private:
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    int insert(string word) {
+        Node* node = root;
+        int cnt = 0;
+        for(int i = 0;i<word.length();i++) {
+            if(!node->containsKey(word[i])) {
+                cnt++;
+                node->putKey(word[i], new Node());
+            }   
+            node = node->getNode(word[i]);
+        }
+        return cnt;
+    }
+};
+
+int countDistinctSubstrings(string &s)
+{
+    Trie trie;
+    int cnt = 0;
+    for(int i=0;i<s.length();i++) {
+        string word = "";
+        for(int j = i;j<s.length();j++) {
+            word+=s[j];
+            cnt += trie.insert(word);
+        }
+    }
+    return cnt+1;
 }
