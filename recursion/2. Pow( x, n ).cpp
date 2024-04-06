1b39379963b0de8e3f43024623976da83f7c3fa3
Dipali Kumbhar <63409376+Dipali742@users.noreply.github.com>
2024-04-06 17:48:28 +0530

    Update 2. Pow( x, n ).cpp

--- recursion/2. Pow( x, n ).cpp
+++ recursion/2. Pow( x, n ).cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n == 0)
-            return  (double)1;
-        
-        if(n < 0)
-            return  (double)myPow(x, n+1)/x;
-        else
-            return  (double) x*myPow(x, n-1);
+        if(n==0) return 1;
+        if(n<0) {
+            n = abs(n);
+            x = 1/x;
+        }
+        if(n%2==0){
+            return myPow(x*x, n/2);
+        }
+        else{
+            return x*myPow(x, n-1);
+        }
     }
 };
