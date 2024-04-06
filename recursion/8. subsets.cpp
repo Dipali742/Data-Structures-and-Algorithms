ff7148abc54e741983195973c73bfbd55330ecc2
Dipali Kumbhar <63409376+Dipali742@users.noreply.github.com>
2024-04-06 21:43:04 +0530

    Update 8. subsets.cpp

--- recursion/8. subsets.cpp
+++ recursion/8. subsets.cpp
@@ -1,3 +1,33 @@
+/*
+    bit manipulation
+    Time complexity - O(2^n * n)
+    Space complexity - O(1)
+*/
+vector<vector<int>> subsets(vector<int>& s) {
+       int n = s.size();
+        vector<vector<int>>ans;
+        ans.push_back({});
+        for (int num = 0; num < (1 << n); num++) {
+            vector<int> sub;
+            for (int i = 0; i < n; i++) {
+                //check if the ith bit is set or not
+                if (num & (1 << i)) {
+                    sub.push_back(s[i]);
+                }
+		    }
+            if (sub.size() > 0) {
+                ans.push_back(sub);
+            }
+	    }
+	    return ans;
+}
+
+/*
+    recursive - take or not_take
+    Time complexity - O(2^n)
+    Space complextiy - O(n)
+*/
+
 class Solution {
 public:
 
