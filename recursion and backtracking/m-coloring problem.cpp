0fec1dfe50edadbf87045bd879634dc2b059b233
Dipali Kumbhar <63409376+Dipali742@users.noreply.github.com>
2024-04-07 20:50:03 +0530

    Update m-coloring problem.cpp

--- recursion and backtracking/m-coloring problem.cpp
+++ recursion and backtracking/m-coloring problem.cpp
@@ -1,13 +1,4 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-class Solution{
-public:
-  
-    bool safeToColor(bool graph[101][101], int node, int c, vector<int>&color, int n) {
+ bool safeToColor(bool graph[101][101], int node, int c, vector<int>&color, int n) {
         for(int i = 0;i<n;i++) {
             if(graph[node][i] && color[i] == c)
                 return false;
@@ -36,31 +27,3 @@ public:
         vector<int>color(n, 0);
         return colored(graph, m, n, 0, color);
     }
-};
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m, e;
-        cin >> n >> m >> e;
-        int i;
-        bool graph[101][101];
-        for (i = 0; i < n; i++) {
-            memset(graph[i], 0, sizeof(graph[i]));
-        }
-        for (i = 0; i < e; i++) {
-            int a, b;
-            cin >> a >> b;
-            graph[a - 1][b - 1] = 1;
-            graph[b - 1][a - 1] = 1;
-        }
-        Solution ob;
-        cout << ob.graphColoring(graph, m, n) << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
