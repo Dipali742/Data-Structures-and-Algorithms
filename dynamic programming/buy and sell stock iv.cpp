0c9f9386ca9779aadbfd1c9b25dd815c4a2c5c0c
Dipali Kumbhar <63409376+Dipali742@users.noreply.github.com>
2024-04-11 22:56:32 +0530

    Create buy and sell stock iv.cpp

---
+++ dynamic programming/buy and sell stock iv.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+         int n = prices.size();
+        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+
+        for(int day = n-1; day >= 0; day--) {
+            for(int buy = 1; buy >=0 ; buy--) {
+                for(int cap = 1; cap < k+1 ; cap++) {
+                    if(buy == 0) {
+                        dp[day][buy][cap] = max(-prices[day] + dp[day+1][1][cap], dp[day+1][0][cap]);
+                    } else {
+                        dp[day][buy][cap] = max(prices[day] + dp[day+1][0][cap-1], dp[day+1][1][cap]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][0][k];
+    }
+};
