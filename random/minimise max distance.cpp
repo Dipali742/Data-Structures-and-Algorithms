47f71d2cb443cbaf63abd64a0183265c890232d1
Dipali Kumbhar <63409376+Dipali742@users.noreply.github.com>
2024-04-13 20:04:05 +0530

    Update minimise max distance.cpp

--- random/minimise max distance.cpp
+++ random/minimise max distance.cpp
@@ -1,8 +1,6 @@
-
-
-
-#include <bits/stdc++.h>
-using namespace std;
+/*
+    using max heap
+*/
 
 long double minimiseMaxDistance(vector<int> &arr, int k) {
     int n = arr.size(); //size of array.
@@ -34,12 +32,46 @@ long double minimiseMaxDistance(vector<int> &arr, int k) {
     return pq.top().first;
 }
 
-int main()
-{
-    vector<int> arr = {1, 2, 3, 4, 5};
-    int k = 4;
-    long double ans = minimiseMaxDistance(arr, k);
-    cout << "The answer is: " << ans << "\n";
-    return 0;
+
+/*
+    using binary search
+*/
+
+int numberOfGasStationsRequired(long double dist, vector<int> &arr) {
+    int n = arr.size(); // size of the array
+    int cnt = 0;
+    for (int i = 1; i < n; i++) {
+        int numberInBetween = ((arr[i] - arr[i - 1]) / dist);
+        if ((arr[i] - arr[i - 1]) == (dist * numberInBetween)) {
+            numberInBetween--;
+        }
+        cnt += numberInBetween;
+    }
+    return cnt;
+}
+
+long double minimiseMaxDistance(vector<int> &arr, int k) {
+    int n = arr.size(); // size of the array
+    long double low = 0;
+    long double high = 0;
+
+    //Find the maximum distance:
+    for (int i = 0; i < n - 1; i++) {
+        high = max(high, (long double)(arr[i + 1] - arr[i]));
+    }
+
+    //Apply Binary search:
+    long double diff = 1e-6 ;
+    while (high - low > diff) {
+        long double mid = (low + high) / (2.0);
+        int cnt = numberOfGasStationsRequired(mid, arr);
+        if (cnt > k) {
+            low = mid;
+        }
+        else {
+            high = mid;
+        }
+    }
+    return high;
 }
 
