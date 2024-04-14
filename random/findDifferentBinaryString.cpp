5eaf13ebed5d86c2117098474749d4326a25db03
Dipali Kumbhar <63409376+Dipali742@users.noreply.github.com>
2024-04-14 11:07:52 +0530

    Create findDifferentBinaryString.cpp

---
+++ random/findDifferentBinaryString.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool generateAllBinaryStrings(int n, int index, string &str, unordered_map<string, int>mp) {
+        if(index == n) {
+            if(mp.find(str) == mp.end()) {
+                return true;
+            }
+            return false;
+        }
+        //recursive calls
+        str+="0";
+        if(generateAllBinaryStrings(n, index+1, str, mp))
+            return true;
+        str.pop_back();
+        str+="1";
+        if(generateAllBinaryStrings(n, index+1, str, mp))
+            return true;
+        str.pop_back();
+        return false;
+    }
+
+    string findDifferentBinaryString(vector<string>& nums) {
+        vector<string>binary_strings;
+        int n = nums.size();
+        unordered_map<string, int>mp;
+        for(auto it: nums)
+            mp[it]++;
+        string ans = "";
+        generateAllBinaryStrings(n,  0, ans,mp);
+        return ans;
+    }
+};
