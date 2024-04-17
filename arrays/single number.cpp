f0bbdb222383b0fc0899d7f4949f72ceddd61ecf
Dipali Kumbhar <63409376+Dipali742@users.noreply.github.com>
2024-04-17 17:28:24 +0530

    Create single number.cpp

---
+++ arrays/single number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int>mp;
+        for(int i =0;i<nums.size();i++) {
+            mp[nums[i]]++;
+        }
+        for(auto it:mp) {
+            if(it.second == 1)
+                return it.first;
+        }
+        return -1;
+    }
+};
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i =0;i<nums.size();i++) {
+            ans = ans^nums[i];
+        }
+        return ans;
+    }
+};
