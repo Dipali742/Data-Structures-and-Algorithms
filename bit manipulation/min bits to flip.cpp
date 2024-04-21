66fe98b8e7c4e30183dd53fa52c3fa7932cb97a4
Dipali Kumbhar <63409376+Dipali742@users.noreply.github.com>
2024-04-21 14:06:50 +0530

    Create min bits to flip.cpp

---
+++ bit manipulation/min bits to flip.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int xorr = start ^ goal;
+        int cnt = 0;
+        for(int i = 0 ; i<32;i++) {
+            if(xorr >> i & 1)
+                cnt++;
+        }
+        return cnt;
+    }
+};
