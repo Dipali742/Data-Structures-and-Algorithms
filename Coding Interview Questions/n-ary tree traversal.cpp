d3a09d33dc981ec38ccb40150236bbec00c1aef3
Dipali Kumbhar <63409376+Dipali742@users.noreply.github.com>
2024-06-01 21:29:52 +0530

    Update n-ary tree traversal.cpp

--- Coding Interview Questions/n-ary tree traversal.cpp
+++ Coding Interview Questions/n-ary tree traversal.cpp
@@ -1,1 +1,36 @@
+/*
+Que1 : What do you know about trees?
+Que2 : Write node structure of Binary trees
+Que3 : What are N-ary trees, write node structure?
+Que4 : Gave one node structure and asked to write pre-order traversal
+*/
 
+class TreeNode {
+public:
+    int data;
+    TreeNode* leftMostChild;
+    TreeNode* rightSibiling;
+    TreeNode* parent;
+}
+
+void preorder(TreeNode* root) {
+   if(root == NULL)
+        return;
+    TreeNode* node = root;
+    bool flg = false;
+    while(1) {
+        if(!flg)
+            cout<<node->data<<endl;
+        if(!flg && node->leftMostChild) {
+            node = node->leftMostChild;
+        } else if(node->rightSibling) {
+            node = node->rightSibling;
+            flg = false;
+        } else {
+            node = node->parent;
+            flg = true;
+            if(node == root)
+                break;
+        } 
+    }
+}
